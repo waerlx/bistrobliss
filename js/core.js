1259b53371262a8a087a247826abbc82e4a381eb
waerlx <adelinaakylbekova565gmail.com>
2023-11-30 21:46:22 +0600

    add css blog

--- js/core.js
+++ js/core.js
@@ -1,2 +1,10 @@
-let name = prompt("enter ypur name")
-alert("Hello" + name)
+// document.querySelector('.js-btn').addEventListener('click', function(){
+//    document.querySelector('.js-btn').classList.add('active')
+//    document.querySelector('.js-btn').style.height   = "200px"
+//    document.querySelector('.js-btn').textContent = "Текст кнопки изменен"
+//    let newhr = document.createElement('<p>Paragraph<p>');
+//    document.querySelector('.js-btn').insertAdjacentElement('aftend', newhr)
+
+// })
+
+// // console.log(123)
